1688a45a564728c24cc6f986f993c1ed85302f48
rcmehedi15 <hmdmehedi2016@gmail.com>
2022-10-18 13:08:54 +0600

    next

--- Day 3/list.py
+++ Day 3/list.py
@@ -1,38 +1,72 @@
-#item added
+# #item added
 
-list=["mehedi","rifat","mamun"]
-list.append("emon")
-print(list)
+# list=["mehedi","rifat","mamun"]
+# list.append("emon")
+# print(list)
 
-#item added any position
+# #item added any position
 
-list=["mehedi","rifat","mamun"]
-list.insert(2,"rana")
-print(list)
+# list=["mehedi","rifat","mamun"]
+# list.insert(2,"rana")
+# print(list)
 
-#item added list by list 
+# #item added list by list 
 
-mylist = ["a","b","c","d"]
-tropical = ["e","f","g","h"]
+# mylist = ["a","b","c","d"]
+# tropical = ["e","f","g","h"]
 
-mylist.extend(tropical)
+# mylist.extend(tropical)
 
-print(mylist)
+# print(mylist)
 
-#item remove
+# #item remove
 
-remove  = ['a','b','c']
-remove.remove('c')
-print(remove)
+# remove  = ['a','b','c']
+# remove.remove('c')
+# print(remove)
 
-#item remove
+# #item remove
 
-popr = ['a','b','c']
-popr.pop(1)
-print(popr)
+# popr = ['a','b','c']
+# popr.pop(1)
+# print(popr)
 
-#item clear
+# #item clear
 
-clearr = ['a','b','c']
-clearr.clear()
-print(clearr)
+# clearr = ['a','b','c']
+# clearr.clear()
+# print(clearr)
+
+
+# #for loop
+
+# forloop = ["hasan", "tana", "mana"]
+# for i in range(len(forloop)):
+#   print(forloop[i])
+
+  #while loop
+
+whileloop = ['you','me','to']
+i = 0
+while i < len(whileloop):
+    print(whileloop[i])
+    i = i +1
+
+
+#join list
+
+join_a = ['anu','monu','conu']
+join_b = ['ranu','janu','manu']
+
+joining_item = join_a + join_b
+
+print(joining_item)
+
+#forjoining
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for joining in list2:
+    list1.append(joining)
+    print(list1)
---
+++ Day 3/sort.py
@@ -0,0 +1,30 @@
+# #assending
+# sortr = ['mehedi','rana','johir']
+
+# sortr.sort()
+# print(sortr)
+
+# #desending
+# sortr = ['mehedi','rana','johir']
+
+# sortr.sort(reverse=True)
+# print(sortr)
+
+# #lower
+
+# lowerr = ["r", "v", "e", "c"]
+
+# lowerr.sort(key = str.lower)
+
+# print(lowerr)
+
+# #reverse
+
+# reversee= ['a','b','v']
+
+# reversee.reverse()
+# print(reversee)
+
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[3:5])
