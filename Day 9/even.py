26a85c407931a8b2991dfe7a8d27b6eb996430a8
rcmehedi15 <hmdmehedi2016@gmail.com>
2022-10-27 16:35:32 +0600

    practice

---
+++ Day 9/even.py
@@ -0,0 +1,6 @@
+number = int(input("Enter The Value : "))
+
+if number % 2 == 0:
+    print("Number Is Even")
+else:
+    print("Number is odd")
---
+++ Day 9/grade.py
@@ -0,0 +1,14 @@
+mark = int(input("Enter number : "))
+
+if mark >= 80:
+    print("Grade A+ ")
+elif mark >=  70:
+    print("Grade A-")
+elif mark >=  60:
+    print("Grade B+")
+elif mark >=  50:
+    print("Grade B")
+elif mark >=  40:
+    print("Grade C")
+else:
+    print(" Student is Fail")
