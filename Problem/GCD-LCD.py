03e439174830c8e4943a7683cc62d776a8f0ef6f
rcmehedi15 <hmdmehedi2016@gmail.com>
2022-10-29 12:08:00 +0600

    GCD and LCM Program practice

---
+++ Problem/GCD-LCD.py
@@ -0,0 +1,10 @@
+#GCD program (Go Sa Guuu)
+import math
+print("The GCD of 60 and 48 is : ",end ="")
+print(math.gcd(60,48))
+
+#LCM program (lo sah guuuu)
+
+import math
+print("The LCM of 60 and 48 is : ",end ="")
+print(math.lcm(60,48))
